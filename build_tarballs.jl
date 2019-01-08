1398ab351a236969e53167ac9ae543f228fd63c7
YuK_Ota <whitenightwinter@gmail.com>
2019-01-08 18:47:15 +0900

    feat: update to v2.0.3

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,11 +2,13 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
+name = "FluidSynth"
+version = v"2.0.3"
 
-# Collection of sources required to build libfluidsynth
+# Collection of sources required to build FluidSynth
 sources = [
     "https://github.com/FluidSynth/fluidsynth.git" =>
-    "6e9d84f02a7a0f7e436c2adffc4a065608f490ba",
+    "1bae9b2fe1a958f54f4910c802a79673e0df9850",
 
 ]
 
@@ -16,7 +18,7 @@ cd $WORKSPACE/srcdir
 cd fluidsynth/
 mkdir build
 cd build/
-cmake ..  -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
 make
 make install
 exit
@@ -31,13 +33,16 @@ platforms = [
 
 # The products that we will ensure are always built
 products(prefix) = [
-    LibraryProduct(prefix, "libfluidsynth", :libfluidsynth)
+    LibraryProduct(prefix, "libfluidsynth", :libfluidsynth),
+    ExecutableProduct(prefix, "fluidsynth", :fluidsynth)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/staticfloat/GlibBuilder/releases/download/v2.54.2-2/build.jl"
+    "https://github.com/staticfloat/GlibBuilder/releases/download/v2.54.2-2/build.jl",
+    "https://github.com/staticfloat/libffiBuilder/releases/download/v3.2.1-0/build.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "FluidSynth", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
